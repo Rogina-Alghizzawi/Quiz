2304566d54cc174c6f33c6fb0e16c9021a934332
Rogina-Alghizzawi <119942227+Rogina-Alghizzawi@users.noreply.github.com>
2024-08-11 20:04:34 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen.js';
+import QuizScreen from './src/screens/Quiz';
+import ResultScreen from './src/screens/ResultScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Quiz" component={QuizScreen} />
+        <Stack.Screen name="Result" component={ResultScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
